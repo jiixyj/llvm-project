526740ff5fd48f4344e59e964e2e818f58d63f5f
Jan Kokemüller <jan.kokemueller@gmail.com>
2021-10-23 12:27:41 +0200

    Revert "[clang-format] fix indent in alignChainedConditionals"
    This reverts commit 5987d7c59da5a7611036c2952f5a1f501d17ad48.

--- clang/lib/Format/WhitespaceManager.cpp
+++ clang/lib/Format/WhitespaceManager.cpp
@@ -793,11 +793,12 @@ void WhitespaceManager::alignChainedConditionals() {
         Changes, /*StartAt=*/0);
   } else {
     static auto AlignWrappedOperand = [](Change const &C) {
-      FormatToken *Previous = C.Tok->getPreviousNonComment();
+      auto Previous = C.Tok->getPreviousNonComment(); // Previous;
       return C.NewlinesBefore && Previous && Previous->is(TT_ConditionalExpr) &&
-             (Previous->is(tok::colon) &&
-              (C.Tok->FakeLParens.size() == 0 ||
-               C.Tok->FakeLParens.back() != prec::Conditional));
+             (Previous->is(tok::question) ||
+              (Previous->is(tok::colon) &&
+               (C.Tok->FakeLParens.size() == 0 ||
+                C.Tok->FakeLParens.back() != prec::Conditional)));
     };
     // Ensure we keep alignment of wrapped operands with non-wrapped operands
     // Since we actually align the operators, the wrapped operands need the
